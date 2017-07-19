d74d79fd0741b83959a535f7f4badddcf0377cc8
Nicolas Dandrimont <nicolas@dandrimont.eu>
2017-07-19 14:04:54 +0200

    tests: add create_tasks test

--- swh/scheduler/tests/test_scheduler.py
+++ swh/scheduler/tests/test_scheduler.py
@@ -3,10 +3,12 @@
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
+import copy
 import datetime
 import os
 import unittest
 
+from arrow import utcnow
 from nose.tools import istest
 import psycopg2
 
@@ -40,6 +42,17 @@ class Scheduler(SingleDbTestFixture, unittest.TestCase):
         tt2['type'] = 'update-hg'
         tt2['description'] = 'Update a mercurial repository'
         tt2['backend_name'] = 'swh.loader.mercurial.tasks.UpdateHgRepository'
+
+        self.task1_template = t1_template = {
+            'type': tt['type'],
+            'arguments': {
+                'args': [],
+                'kwargs': {},
+            },
+            'next_run': None,
+        }
+        self.task2_template = t2_template = copy.deepcopy(t1_template)
+        t2_template['type'] = tt2['type']
 
     def tearDown(self):
         self.backend.db.close()
@@ -76,3 +89,41 @@ class Scheduler(SingleDbTestFixture, unittest.TestCase):
         self.backend.create_task_type(tt)
         self.backend.create_task_type(tt2)
         self.assertCountEqual([tt2, tt], self.backend.get_task_types())
+
+    @staticmethod
+    def _task_from_template(template, next_run, *args, **kwargs):
+        ret = copy.deepcopy(template)
+        ret['next_run'] = next_run
+        if args:
+            ret['arguments']['args'] = list(args)
+        if kwargs:
+            ret['arguments']['kwargs'] = kwargs
+        return ret
+
+    def _create_task_types(self):
+        self.backend.create_task_type(self.task_type)
+        self.backend.create_task_type(self.task_type2)
+
+    @istest
+    def create_tasks(self):
+        self._create_task_types()
+        next_run = utcnow()
+        tasks = [
+            self._task_from_template(self.task1_template, next_run,
+                                     'argument-%03d' % i)
+            for i in range(100)
+        ]
+
+        ret = self.backend.create_tasks(tasks)
+        ids = set()
+        for task, orig_task in zip(ret, tasks):
+            task = copy.deepcopy(task)
+            self.assertNotIn(task['id'], ids)
+            self.assertEqual(task['status'], 'next_run_not_scheduled')
+            self.assertEqual(task['current_interval'],
+                             self.task_type['default_interval'])
+            ids.add(task['id'])
+            del task['id']
+            del task['status']
+            del task['current_interval']
+            self.assertEqual(task, orig_task)
