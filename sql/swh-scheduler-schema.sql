3194c2bfc83954f9db78f8be6544bb4260e90604
Nicolas Dandrimont <nicolas@dandrimont.eu>
2017-07-25 18:49:17 +0200

    sql: new logic for recurrence of one-shot tasks

--- sql/swh-scheduler-schema.sql
+++ sql/swh-scheduler-schema.sql
@@ -35,7 +35,7 @@ comment on column task_type.max_queue_length is 'Maximum length of the queue for
 comment on column task_type.num_retries is 'Default number of retries on transient failures';
 comment on column task_type.retry_delay is 'Retry delay for the task';
 
-create type task_status as enum ('next_run_not_scheduled', 'next_run_scheduled', 'disabled');
+create type task_status as enum ('next_run_not_scheduled', 'next_run_scheduled', 'disabled', 'completed');
 comment on type task_status is 'Status of a given task';
 
 create type task_policy as enum ('recurring', 'oneshot');
@@ -224,54 +224,76 @@ as $$
   returning *;
 $$;
 
-create or replace function swh_scheduler_compute_new_task_interval (task_type text,
-                                                                    current_interval interval,
-                                                                    end_status task_run_status)
-  returns interval
-  language plpgsql
-  stable
-as $$
-declare
-  task_type_row task_type%rowtype;
-  adjustment_factor float;
-begin
-  select *
-    from task_type
-    where type = swh_scheduler_compute_new_task_interval.task_type
-  into task_type_row;
-
-  case end_status
-  when 'eventful' then
-    adjustment_factor := 1/task_type_row.backoff_factor;
-  when 'uneventful' then
-    adjustment_factor := task_type_row.backoff_factor;
-  else
-    -- failed or lost task: no backoff.
-    adjustment_factor := 1;
-  end case;
-
-  return greatest(task_type_row.min_interval,
-                  least(task_type_row.max_interval,
-                        adjustment_factor * current_interval));
-end;
-$$;
-
-create or replace function swh_scheduler_update_task_interval ()
+create or replace function swh_scheduler_update_task_on_task_end ()
   returns trigger
   language plpgsql
 as $$
+declare
+  cur_task task%rowtype;
+  cur_task_type task_type%rowtype;
+  adjustment_factor float;
+  new_interval interval;
 begin
-  update task
-    set status = 'next_run_not_scheduled',
-        current_interval = swh_scheduler_compute_new_task_interval(type, current_interval, new.status),
-        next_run = now () + swh_scheduler_compute_new_task_interval(type, current_interval, new.status)
-    where id = new.task;
+  select * from task where id = new.task into cur_task;
+  select * from task_type where type = cur_task.type into cur_task_type;
+
+  case
+    when new.status = 'permfailed' then
+      update task
+        set status = 'disabled'
+        where id = cur_task.id;
+    when new.status in ('eventful', 'uneventful') then
+      case
+        when cur_task.policy = 'oneshot' then
+          update task
+            set status = 'completed'
+            where id = cur_task.id;
+        when cur_task.policy = 'recurring' then
+          if new.status = 'uneventful' then
+            adjustment_factor := 1/cur_task_type.backoff_factor;
+          else
+            adjustment_factor := 1/cur_task_type.backoff_factor;
+          end if;
+          new_interval := greatest(
+            cur_task_type.min_interval,
+            least(
+              cur_task_type.max_interval,
+              adjustment_factor * cur_task.current_interval));
+          update task
+            set status = 'next_run_not_scheduled',
+                next_run = now() + new_interval,
+                interval = new_interval,
+                retries_left = cur_task_type.max_retries
+            where id = cur_task.id;
+      end case;
+    else -- new.status in 'failed', 'lost'
+      if coalesce(cur_task.retries_left, 0) > 0 then
+        update task
+          set status = 'next_run_not_scheduled',
+              next_run = now() + cur_task_type.retry_delay,
+              retries_left = cur_task.retries_left - 1
+          where id = cur_task.id;
+      else -- no retries left
+        case
+          when cur_task.policy = 'oneshot' then
+            update task
+              set status = 'disabled'
+              where id = cur_task.id;
+          when cur_task.policy = 'recurring' then
+            update task
+              set status = 'next_run_not_scheduled',
+                  next_run = now() + cur_task.current_interval,
+                  retries_left = cur_task_type.max_retries
+              where id = cur_task.id;
+        end case;
+      end if; -- retries
+  end case;
   return null;
 end;
 $$;
 
-create trigger update_interval_on_task_end
+create trigger update_task_on_task_end
   after update of status on task_run
   for each row
-  when (new.status IN ('eventful', 'uneventful', 'failed', 'lost'))
-  execute procedure swh_scheduler_update_task_interval ();
+  when (new.status NOT IN ('scheduled', 'started'))
+  execute procedure swh_scheduler_update_task_on_task_end ();
