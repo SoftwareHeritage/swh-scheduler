0e8e237e856fdc9a676de9e30614e6a03f11f307
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-03-09 16:51:04 +0100

    sql/data: Add new task type for loading mercurial dump

--- sql/swh-scheduler-data.sql
+++ sql/swh-scheduler-data.sql
@@ -63,3 +63,16 @@ values (
        'swh.loader.mercurial.tasks.LoadMercurialTsk',
        '1 day', '1 day', '1 day', 1,
        1000);
+
+insert into task_type(
+       type,
+       description,
+       backend_name,
+       default_interval, min_interval, max_interval, backoff_factor,
+       max_queue_length)
+values (
+       'origin-load-archive-hg',
+       'Loading archive mercurial repository swh-loader-mercurial',
+       'swh.loader.mercurial.tasks.LoadArchiveMercurialTsk',
+       '1 day', '1 day', '1 day', 1,
+       1000);
