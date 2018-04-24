9e13fd2d06bb1431713fddc90c195c4153c89470
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-04-24 15:40:33 +0200

    swh.scheduler.api: Bootstrap scheduler's remote api
    Related T1034

---
+++ swh/scheduler/api/client.py
@@ -0,0 +1,96 @@
+# Copyright (C) 2018  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+
+from swh.core.api import SWHRemoteAPI
+
+
+class SchedulerAPIError(Exception):
+    """Specific internal scheduler api issue (mainly connection)
+
+    """
+
+    def __str__(self):
+        args = self.args
+        return 'An unexpected error occurred in the api backend: %s' % args
+
+
+class RemoteScheduler(SWHRemoteAPI):
+    """Proxy to a remote scheduler API
+
+    """
+    def __init__(self, url):
+        super().__init__(api_exception=SchedulerAPIError, url=url)
+
+    def create_task_type(self, task_type):
+        return self.post('create_task_type', {'task_type': task_type})
+
+    def get_task_type(self, task_type_name):
+        return self.post('get_task_type', {'task_type_name': task_type_name})
+
+    def get_task_types(self):
+        return self.post('get_task_types', {})
+
+    def create_tasks(self, tasks):
+        return self.post('create_tasks', {'tasks': tasks})
+
+    def disable_tasks(self, task_ids):
+        return self.post('disable_tasks', {'task_ids': task_ids})
+
+    def get_tasks(self, task_ids):
+        return self.post('get_tasks', {'task_ids': task_ids})
+
+    def peek_ready_tasks(self, task_type, timestamp=None, num_tasks=None):
+        return self.post('peek_ready_tasks', {
+            'task_type': task_type,
+            'timestamp': timestamp,
+            'num_tasks': num_tasks,
+        })
+
+    def grab_ready_tasks(self, task_type, timestamp=None, num_tasks=None):
+        return self.post('grab_ready_tasks', {
+            'task_type': task_type,
+            'timestamp': timestamp,
+            'num_tasks': num_tasks,
+        })
+
+    def schedule_task_run(self, task_id, backend_id, metadata=None,
+                          timestamp=None):
+        return self.post('schedule_task_run', {
+            'task_id': task_id,
+            'backend_id': backend_id,
+            'metadata': metadata,
+            'timestamp': timestamp,
+        })
+
+    def mass_schedule_task_runs(self, task_runs):
+        return self.post('mass_schedule_task_runs', {'task_runs': task_runs})
+
+    def start_task_run(self, backend_id, metadata=None, timestamp=None):
+        return self.post('start_task_run', {
+            'backend_id': backend_id,
+            'metadata': metadata,
+            'timestamp': timestamp,
+        })
+
+    def end_task_run(self, backend_id, status, metadata=None, timestamp=None):
+        return self.post('end_task_run', {
+            'backend_id': backend_id,
+            'status': status,
+            'metadata': metadata,
+            'timestamp': timestamp,
+        })
+
+    def filter_task_to_archive(self, after_ts, before_ts, limit=10,
+                               last_id=-1):
+        return self.post('filter_task_to_archive', {
+            'after_ts': after_ts,
+            'before_ts': before_ts,
+            'limit': limit,
+            'last_id': last_id,
+        })
+
+    def delete_archived_tasks(self, task_ids):
+        return self.post('delete_archived_tasks', {'task_ids': task_ids})
