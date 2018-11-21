5c636ef95e828ac0080961e4ba56bca6de05265d
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-11-21 13:00:13 +0100

    data/sql: Insert new task type for origin indexer
    Related T1326

--- swh/scheduler/sql/40-swh-data.sql
+++ swh/scheduler/sql/40-swh-data.sql
@@ -245,3 +245,16 @@ values (
        'swh.indexer.tasks.ContentRangeFossologyLicense',
        '1 day', '12:00:00', '1 days', 2,
        5000);
+
+insert into task_type(
+       type,
+       description,
+       backend_name,
+       default_interval, min_interval, max_interval, backoff_factor,
+       max_queue_length)
+values (
+       'indexer_origin_head',
+       'Origin Head indexer task',
+       'swh.indexer.tasks.OriginHead',
+       '1 day', '12:00:00', '1 days', 2,
+       5000);
