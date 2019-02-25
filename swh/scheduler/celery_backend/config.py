40d32b4eb78f6fa3ee0848e028437c0598ea11e9
David Douard <david.douard@sdfa3.org>
2019-02-25 13:57:46 +0100

    celery: fix the celery config to ensure the default 'celery' queue exists
    as it used to be before the recent refactorings.

--- swh/scheduler/celery_backend/config.py
+++ swh/scheduler/celery_backend/config.py
@@ -227,8 +227,6 @@ CELERY_DEFAULT_CONFIG = dict(
     worker_disable_rate_limits=True,
     # Task routing
     task_routes=route_for_task,
-    # Task queues this worker will consume from
-    task_queues=CELERY_QUEUES,
     # Allow pool restarts from remote
     worker_pool_restarts=True,
     # Do not prefetch tasks
@@ -245,8 +243,9 @@ def build_app(config=None):
         {k: v for (k, (_, v)) in DEFAULT_CONFIG.items()},
         config or {})
 
-    config['task_queues'] = [Queue(queue, Exchange(queue), routing_key=queue)
-                             for queue in config.get('task_queues', ())]
+    config['task_queues'] = CELERY_QUEUES + [
+        Queue(queue, Exchange(queue), routing_key=queue)
+        for queue in config.get('task_queues', ())]
     logger.debug('Creating a Celery app with %s', config)
 
     # Instantiate the Celery app
