2044c07dde51055ae8f5f993712876509dc0545e
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-06-28 17:52:57 +0200

    data: Insert new cgit instance lister task
    Related T1835

--- swh/scheduler/sql/50-swh-data.sql
+++ swh/scheduler/sql/50-swh-data.sql
@@ -388,3 +388,16 @@ values (
        '1 week',
        '1 week',
        '1 week', 1);
+
+insert into task_type(
+       type,
+       description,
+       backend_name,
+       default_interval, min_interval, max_interval, backoff_factor)
+values (
+       'list-cgit',
+       'CGit instance lister',
+       'swh.lister.cgit.tasks.CGitListerTask',
+       '64 days',
+       '64 days',
+       '64 days', 1);
