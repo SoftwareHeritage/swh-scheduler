9c2b3299814d4005cfcc788ea6fa935ae96ab00f
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-09-03 10:18:30 +0200

    50-swh-data: Add load-tar task-type
    This is needed to be in sync with the docker-dev environment.

--- swh/scheduler/sql/50-swh-data.sql
+++ swh/scheduler/sql/50-swh-data.sql
@@ -401,3 +401,16 @@ values (
        '64 days',
        '64 days',
        '64 days', 1);
+
+insert into task_type(
+       type,
+       description,
+       backend_name,
+       default_interval, min_interval, max_interval, backoff_factor, num_retries)
+values (
+       'load-tar',
+       'Load Tar from gnu',
+       'swh.loader.package.tasks.LoadGNU',
+       '64 days',
+       '64 days',
+       '64 days', 1, 3);
