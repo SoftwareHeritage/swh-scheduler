c2ccf460f5528fda8964ad3c0e35096da9175ce2
David Douard <david.douard@sdfa3.org>
2019-10-16 10:43:03 +0200

    updater/tests: rewrite updater's tests as pytest functions
    The way the scheduler_db and updater_db fixtures are built is not very
    straighforward nor satisfying, but it works.

--- swh/scheduler/tests/updater/conftest.py
+++ swh/scheduler/tests/updater/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import glob
 import os
+from arrow import utcnow  # XXX
 
 from swh.core.utils import numfile_sortkey as sortkey
 from swh.scheduler.updater.backend import SchedulerUpdaterBackend
@@ -12,13 +13,9 @@ DUMP_FILES = os.path.join(SQL_DIR, 'updater', '*.sql')
 
 
 @pytest.fixture
-def swh_scheduler_updater(request, postgresql_proc, postgresql):
+def swh_scheduler_updater(postgresql):
     config = {
-        'db': 'postgresql://{user}@{host}:{port}/{dbname}'.format(
-            host=postgresql_proc.host,
-            port=postgresql_proc.port,
-            user='postgres',
-            dbname='tests')
+        'db': postgresql.dsn,
     }
 
     all_dump_files = sorted(glob.glob(DUMP_FILES), key=sortkey)
@@ -31,3 +28,41 @@ def swh_scheduler_updater(request, postgresql_proc, postgresql):
 
     backend = SchedulerUpdaterBackend(**config)
     return backend
+
+
+def make_event(event_type, name, origin_type):
+    return {
+        'type': event_type,
+        'repo': {
+            'name': name,
+        },
+        'created_at': utcnow(),
+        'origin_type': origin_type,
+    }
+
+
+def make_simple_event(event_type, name, origin_type):
+    return {
+        'type': event_type,
+        'url': 'https://fakeurl/%s' % name,
+        'origin_type': origin_type,
+        'created_at': utcnow(),
+    }
+
+
+def make_events(events):
+    for event_type, repo_name, origin_type in events:
+        yield make_event(event_type, repo_name, origin_type)
+
+
+def make_incomplete_event(event_type, name, origin_type,
+                          missing_data_key):
+    event = make_event(event_type, name, origin_type)
+    del event[missing_data_key]
+    return event
+
+
+def make_incomplete_events(events):
+    for event_type, repo_name, origin_type, missing_data_key in events:
+        yield make_incomplete_event(event_type, repo_name,
+                                    origin_type, missing_data_key)
