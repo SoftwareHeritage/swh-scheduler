8ec34fe977bd2d2cd547fff39475fa21482b0287
Nicolas Dandrimont <nicolas@dandrimont.eu>
2019-10-23 10:40:37 +0200

    Remove collect_ignore from conftest.py
    This got solved when we started using the shared_task decorator instead of
    instantiating our own app.

--- conftest.py
+++ conftest.py
@@ -4,9 +4,3 @@ from hypothesis import settings
 # https://hypothesis.readthedocs.io/en/latest/settings.html#settings-profiles
 settings.register_profile("fast", max_examples=5, deadline=5000)
 settings.register_profile("slow", max_examples=20, deadline=5000)
-
-# Modules that should not be loaded by --doctest-modules
-collect_ignore = [
-    # NotImplementedError: save_group is not supported by this backend.
-    'swh/scheduler/tests/tasks.py',
-]
