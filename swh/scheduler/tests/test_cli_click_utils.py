7d3cb4fa5ee8d424255c93594466841fe27cf362
Antoine Lambert <anlambert@softwareheritage.org>
2026-07-01 14:56:40 +0200

    tests: Fix tests collection errors when using pytest-xdist for good
    Use fixed dates so test parameters are the same in each pytest-xdist
    worker.

--- swh/scheduler/tests/test_cli_click_utils.py
+++ swh/scheduler/tests/test_cli_click_utils.py
@@ -3,20 +3,22 @@
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
-from datetime import date, datetime, timezone
+from datetime import datetime, timezone
 from email.utils import format_datetime
 
 import pytest
 
 from swh.scheduler.cli.click_utils import DATETIME
 
+BASE_DATE = datetime(year=2026, month=1, day=2, hour=3, minute=4, tzinfo=timezone.utc)
+
 
 def iso(timespec):
     if timespec == "days":
-        now = date.today()
+        now = BASE_DATE.date()
         args = {}
     else:
-        now = datetime.now(timezone.utc)
+        now = BASE_DATE
         args = {"timespec": timespec}
     return now.isoformat(**args)
 
@@ -30,9 +32,9 @@ iso_timespecs = "days hours minutes seconds milliseconds microseconds".split()
         list(iso(spec) for spec in iso_timespecs)
         + [
             # ISO 8601 UTC with Z
-            datetime.now(timezone.utc).isoformat().replace("+00:00", "Z"),
+            BASE_DATE.isoformat().replace("+00:00", "Z"),
             # RFC 822/2822/5322
-            format_datetime(datetime.now(timezone.utc)),
+            format_datetime(BASE_DATE),
             # dateutil can handle this
             "1st Jan 2027",
             # dateparser can handle this
